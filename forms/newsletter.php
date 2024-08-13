7095dbba9b10bc4d6a779e782587ebada04698a3
ajith-huho <ajithkskallar@outlook.com>
2024-08-13 21:43:53 +0530

    email form conected

--- forms/newsletter.php
+++ forms/newsletter.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php -->
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -7,21 +7,21 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['email'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject ="New Subscription: " . $_POST['email'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['email'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject ="New Subscription: " . $_POST['email'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,7 +33,7 @@
   );
   */
 
-  $contact->add_message( $_POST['email'], 'Email');
+//   $contact->add_message( $_POST['email'], 'Email');
 
-  echo $contact->send();
-?>
+//   echo $contact->send();
+// ?>
